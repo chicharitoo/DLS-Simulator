dc54c8cc12b9d39a1efffb61a5d7a657ea0ebcbb
chicharito <chicharito@gmail.com>
2025-11-27 15:31:16 -0700

    Add GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/DLS-Simulator/' : '/',
   plugins: [
     react(),
     wasm(),
